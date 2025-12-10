4e35bb7329f8af40f6b0baebb78916d5225accc7
Harikrishnan A S <harikrishnan.annappilly@gmail.com>
2025-12-10 20:17:35 +0530

    Added index page

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 
 app = Flask(__name__)
 
-app_start_time = datetime.utcnow()
+now = datetime.now().astimezone().replace(microsecond=0)
 
 
 @app.route("/")
 def index():
-    return f"Welcome to My App {app_start_time}"
+    return render_template("index.html", app_start_time=now.strftime("%Y-%m-%d %H:%M:%S %Z %z"))
 
 
 if __name__ == "__main__":
