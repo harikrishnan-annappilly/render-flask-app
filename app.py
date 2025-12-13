a11725d33aa4027b5c5cbbbdad31bd2d044738a7
Harikrishnan A S <harikrishnan.annappilly@gmail.com>
2025-12-13 10:58:52 +0530

    Added extra pages

--- app.py
+++ app.py
@@ -11,5 +11,20 @@ def index():
     return render_template("index.html", app_start_time=now.strftime("%Y-%m-%d %H:%M:%S %Z %z"))
 
 
+@app.route("/login")
+def login():
+    return render_template("user/login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("user/register.html")
+
+
+@app.route("/users")
+def users():
+    return render_template("user/users.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
